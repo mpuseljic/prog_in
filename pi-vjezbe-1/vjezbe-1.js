fe1985ca25b2dbc62b4ab88d5c547116c0d0a3e6
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-03-07 21:44:43 +0100

    Create vjezbe-1.js

---
+++ pi-vjezbe-1/vjezbe-1.js
@@ -0,0 +1,24 @@
+
+// 1.zadatak
+for (let i = 1; i < 101; i++){
+    if (i%15 == 0) console.log("FizzBuzz");
+    else if(i%3 == 0) console.log("Fizz");
+    else if(i%5 == 0) console.log("Buzz");
+    else console.log(i);
+
+}
+
+// 2.zadatak
+
+function Zbroj(){
+    var total = 0;
+    for(let i = 1; i<=100; i++){
+        total += i;
+    }
+    return total;
+}
+
+let result = Zbroj.call();
+console.log("Zbroj brojeva od 1 do 100 je: ", result);
+
+// 3.zadatak
