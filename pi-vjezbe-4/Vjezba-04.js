a4649648766f2fcc32e6c9af4d37b848432817f7
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-03-22 20:05:09 +0100

    Update Vjezba-04.js

--- pi-vjezbe-4/Vjezba-04.js
+++ pi-vjezbe-4/Vjezba-04.js
@@ -55,18 +55,119 @@
 // myArray2[1] = "ananas";
 // console.log(myArray2);
 
-const originalArray = [1, 2, 3];
 
-const shallowCopyArray = originalArray;
-const shallowCopyArray2 = Array.from(originalArray);
+// ----------------
+// const originalArray = [1, 2, 3];
 
-const deepCopyArray = JSON.parse(JSON.stringify(originalArray))
+// const shallowCopyArray = originalArray;
+// const shallowCopyArray2 = Array.from(originalArray);
 
-shallowCopyArray[0] = 4;
-shallowCopyArray2[0] = 4;
-deepCopyArray[0] = 4;
-deepCopyArray[0];
+// const deepCopyArray = JSON.parse(JSON.stringify(originalArray))
 
-console.log(originalArray);
-console.log(shallowCopyArray);
-console.log(deepCopyArray);
+// shallowCopyArray[0] = 4;
+// shallowCopyArray2[0] = 4;
+// deepCopyArray[0] = 5;
+
+
+// console.log(originalArray);
+// console.log(shallowCopyArray);
+// console.log(deepCopyArray);
+// console.log(originalArray)
+
+// --------------------
+
+// let numbers = [1, 2, 3, 4, 5];
+// let squares = numbers.map((numb) => {
+//     return numb * numb;
+// })
+// console.log(squares);
+
+// let squares2 = []
+// for(const numb of numbers){
+//     squares.push(numb*numb)
+
+// }
+// console.log(squares2)
+
+// -------
+// 1. Kreiraj polje koje sadrži brojeve od 1 do 10 i ispiši sve parove brojeva ciji je zbroj jednak broju 10.
+
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let target = 10;
+
+for(let i = 0; i< number.length; i++){
+    for(let j = i + 1; j < number.length; j++){
+        if(number[i] + number[j] == target){
+        console.log(number[i], number[j])
+        }
+    }
+}
+
+// 2. Sastavi listu 6 studenata na nekom kolegiju sa sljedećim svojstvima: name, age, gender, grade.
+//Kreiraj funkciju koja vraća sve podatke najbolje studentice na kolegiju.
+
+let students = [
+
+    {name: 'John', age:21 , gender:'male' , grade:85 },
+    {name: 'Mary', age:19 , gender:'female' , grade:92 },
+    {name: 'David', age:25 , gender:'male', grade:78 },
+    {name: 'Ana', age:22 , gender: 'female', grade:89 },
+]
+
+function findTopStudent(){
+    let topGrade = 0;
+    let topFemaleStudent = null;
+
+    for(let student of students){
+        if(student.gender === 'female' && student.grade > topGrade){
+            topGrade = student.grade;
+            topFemaleStudent = student;
+        }
+    }
+    console.log(topFemaleStudent)
+}
+
+
+// 3. Svaki element u zadanom polju objekata transformiraju na način da mu dodas svojstvo "sales" koje ce sadrzavati random broj prodanih primjeraka 
+// knjiga. 
+
+const books = [
+    {title: "The Great Gatsby", author:"F.Scott Fitzgerald" , publicationYear: 1925 },
+    {title: "To Kill a Mockingbird", author: "Harper Lee", publicationYear: 1960 },
+]
+
+for(let book of books){
+    book["sales"] = Math.floor(Math.random() * 100);
+}
+console.log(books)
+//--------
+const booksWithSales = books.map(book => {
+    return {
+        title: book.title,
+        author: book.author,
+        publicationYear: book.publicationYear,
+        sales: Math.floor(Math.random() * 100)
+    }
+
+})
+console.log(booksWithSales)
+
+// ----------------
+// 4. Naslov knjige koja je izdana 1925
+let bookTitle = books.filter((book) => {
+    return book.publicationYear == 1925
+}).map((book) => {
+    return book.title;
+})
+
+console.log(bookTitle);
+
+// 5. Iskoristi funkciju reduce tako da ugnijezdeno polje pretvoris u jedinstveno.
+
+const myArray = [[1, 2], [3,4] , [5,6]];
+
+const flattenedArray = myArray.reduce((accumulator, currentValue) => {
+    return accumulator.concat(currentValue);
+}, []);
+
+console.log(flattenedArray)
