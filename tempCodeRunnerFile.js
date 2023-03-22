9cd7b06daa7d3d2a686f5f3231dba0bff7dff308
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-03-22 19:19:02 +0100

    Create tempCodeRunnerFile.js

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+
+// function Podatci(){
+//     let studentNovi = {
+//         ime: '',
+//         datum_rodenja:"0"
+//     }
+//     for(let student of studenti){
+//         if(student.datum_rodenja > studentNovi.datum_rodenja){
+//         studentNovi.ime = student.ime
+//         studentNovi.datum_rodenja = student.datum_rodenja
+//     }  
+//   }
+   
+//     return studentNovi
+// }
+
+// console.log(Podatci());
