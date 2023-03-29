d6a48d0698b782cfe6ff990c8be83b77164929a6
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-03-29 16:02:22 +0200

    Update vjezba-kontrolna-1.js

--- vjezba-kontrolna-1.js
+++ vjezba-kontrolna-1.js
@@ -793,3 +793,282 @@ function upisanaGodina (){
 }
 
 upisanaGodina()
+
+
+// /////////////////////////////////////////////////////////////////////
+// ///////////////////////// PONAVLJANJE //////////////////////////////
+// ///////////////////////////////////////////////////////////////////
+
+// //1. Napravi funkciju koja prima podatke (možeš ih kopirati iz students.js ) te vraća polje
+// //( Array ) koje u sebi sadrži samo imena i prezimena ( string ) svih studenata
+function Podaci(){
+  let imenaPrezimena = []
+  for(let student of studenti){
+    imenaPrezimena.push(student.ime, student.prezime)
+  }
+  console.log(imenaPrezimena)
+}
+
+Podaci()
+
+// 2. Napravi f-iju koja vraća polje studenata koji su rođeni u Puli
+function rodeniPula(){
+  let rodeni = []
+  for(let student of studenti){
+    if(student.mjesto_rodenja.grad === "Pula"){
+      rodeni.push(student.ime)
+    }
+  }
+  console.log(rodeni)
+}
+
+rodeniPula()
+
+// 3. … a sada f-iju koja broji koliko studenata nije rođeno u Puli
+function neRodeniPula(){
+  let brojac = 0
+  for(let student of studenti){
+    if(student.mjesto_rodenja.grad !== "Pula"){
+      brojac += 1
+    }
+  }
+  console.log(brojac)
+}
+
+neRodeniPula()
+
+// 4. … te f-iju koja vraća najmlađeg studenta:
+function najmladiStudent (){
+  let najmladi = {
+    ime: "",
+    datum_rodenja: "0"
+  }
+  for(let student of studenti){
+    if(student.datum_rodenja > najmladi.datum_rodenja){
+      najmladi.ime = student.ime
+      najmladi.datum_rodenja = student.datum_rodenja
+    }
+  }
+  return najmladi
+}
+
+console.log(najmladiStudent())
+
+// 5. izdvojiti imena studenata i koliko puta su upisali studij (gleda se svaka godina upisanih studija).
+function brojUpisanihStudija (){
+  let studentNovi = []
+  for(let student of studenti){
+    studentNovi.push({
+      ime: student.ime,
+      godine_studija: student.studiji.length
+    })
+  }
+  console.log(studentNovi)
+}
+
+brojUpisanihStudija()
+
+// 6. izdvojiti imena studenata i nastavnu godinu koju je student upisao 2019./2020. akademske godine.
+function upisanaGodina(){
+  let studentNovi = []
+  for(let student of studenti){
+    let studij = student.studiji.filter((studij) => {return studij.akademska_godina === "2019./2020."})
+    if(studij.length){
+      studentNovi.push({
+        ime: student.ime,
+        nastavna_godina: studij[0].nastavna_godina
+      })
+    }
+
+  }
+  console.log(studentNovi)
+}
+
+upisanaGodina()
+
+ // 1. Kreiraj polje koje sadrži brojeve od 1 do 10 i ispiši sve parove brojeva ciji je zbroj jednak broju 10.
+ let polje = [1,2,3,4,5,6,7,8,9,10]
+ let zbroj = 10
+
+ for(let i = 0; i<polje.length; i++){
+  for(let j = i + 1; j<polje.length; j++){
+    if(polje[i] + polje[j] === zbroj){
+      console.log(polje[i], polje[j])
+    }
+  }
+ }
+
+// 2. Sastavi listu 6 studenata na nekom kolegiju sa sljedećim svojstvima: name, age, gender, grade.
+//Kreiraj funkciju koja vraća sve podatke najbolje studentice na kolegiju.
+
+
+let students = [
+
+  {name: 'John', age:21 , gender:'male' , grade:85 },
+  {name: 'Mary', age:19 , gender:'female' , grade:92 },
+  {name: 'David', age:25 , gender:'male', grade:78 },
+  {name: 'Ana', age:22 , gender: 'female', grade:89 },
+]
+
+function najboljaStudentica(){
+  let najboljaOcjena = 0
+  let najbolja = null
+
+  for(let student of students){
+    if(student.gender === 'female' && student.grade > najboljaOcjena){
+      najboljaOcjena = student.grade
+      najbolja = student
+    }
+  }
+  console.log(najbolja)
+}
+
+najboljaStudentica()
+
+// 3. Svaki element u zadanom polju objekata transformiraju na način da mu dodas svojstvo "sales" koje ce sadrzavati random broj prodanih primjeraka 
+// knjiga. 
+
+const books = [
+  {title: "The Great Gatsby", author:"F.Scott Fitzgerald" , publicationYear: 1925 },
+  {title: "To Kill a Mockingbird", author: "Harper Lee", publicationYear: 1960 },
+]
+
+function randomBrojProdanihPrimjeraka(){
+  for(let book of books){
+    book["sales"] = Math.floor(Math.random() * 100)
+  }
+  console.log(books)
+}
+
+randomBrojProdanihPrimjeraka()
+
+
+// 4. Naslov knjige koja je izdana 1925
+
+let bookTitle = books.filter((book) => {return book.publicationYear === 1925}).map((book) => {return book.title})
+console.log(bookTitle)
+
+// 5. Iskoristi funkciju reduce tako da ugnijezdeno polje pretvoris u jedinstveno.
+
+const myArray2 = [[1, 2], [3,4] , [5,6]];
+
+const jedinstveno = myArray2.reduce((previousValue, currentValue) => {return previousValue.concat(currentValue)})
+console.log(jedinstveno)
+
+
+
+// VJEZBA 1 - ZADACI
+//1.zadatak
+for(let i = 1; i<101; i++){
+  if(i%15 === 0){
+    console.log("FizzBuzz")
+  } else if(i%3 === 0){
+    console.log("Fizz")
+  } else if (i%5 === 0){
+      console.log("Buzz")
+  }else{
+    console.log(i)
+  }
+}
+
+//2. zadatak
+function Zbroj(){
+  let zbroj = 0
+  for(let i = 1; i<101; i++){
+    zbroj += i
+  }
+  return zbroj
+}
+
+console.log(Zbroj())
+
+// 3. zadatak 
+function Kvadriraj (parametar){
+  return parametar*parametar
+}
+
+console.log(Kvadriraj(6))
+
+// 4.zadatak 
+// Napisi rekurzivnu funkciju koja iterira sve dok ne dostigne vrijednost drugog parametra
+
+function IWillSayWhenYouCanStop(prvi, drugi){
+  if(drugi >= prvi){
+    return
+  }
+  console.log(drugi)
+  IWillSayWhenYouCanStop(prvi, drugi + 1)
+}
+
+IWillSayWhenYouCanStop(10,0)
+
+// --------------------------
+// 2.VJEŽBA
+// 1. Preokreni string
+function preokreniString (unesiString){
+  let temp = "";
+  for(let i = unesiString.length - 1; i>=0; i--){
+    temp += unesiString[i]
+  }
+  return temp
+}
+
+console.log(preokreniString("abc"))
+
+//2. Je li sting palindrom?
+function jePalindrom (string){
+  for(let i = 0; i<string.length/2; i++){
+    if(string[i] !== string[string.length - 1 - i]){
+      return false
+    }
+  }
+  return true
+}
+
+console.log(jePalindrom("ana"))
+
+// 3. Makni odredeno slovo iz stringa
+function makniSlovo(string, slovo){
+  let novi = ""
+  for(let i = 0; i<string.length; i++){
+    if(string[i] !== slovo){
+      novi += string[i]
+    }
+
+  }
+  return novi
+}
+console.log(makniSlovo("aaaabdrefca" , "a"))
+
+// 4. Makni vise odredenih slova 
+function ukloniSlova(string, slova){
+  let noviString = ""
+  for(let i = 0; i<string.length; i++){
+    if(!slova.includes(string[i])){
+      noviString += string[i]
+    }
+  }
+  return noviString
+}
+
+console.log(ukloniSlova("aabbbcderf", ["a" , "b"]))
+
+// 5. Ispisi slova koja se ponavljaju 
+function ispisiPonavljajucaSlova(string){
+  let ponavljajuci = ""
+  for(let i = 0; i<string.length; i++){
+    let brojac = 0
+    for(let j = 0; j<string.length; j++){
+      if(string[i] === string[j]){
+        brojac += 1
+      }
+    }
+    if(brojac > 1 && !ponavljajuci.includes(string[i])){
+      ponavljajuci += string[i]
+    }
+
+  }
+  return ponavljajuci
+}
+
+console.log(ispisiPonavljajucaSlova("aaabbbcderf"))
